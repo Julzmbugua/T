3d5fc5197dc33d1e9ba70821c8f6efa1b21f1aed
Julius Mbugua <julzmbugua@gmail.com>
2017-03-12 18:21:47 +0300

    switched to postgres

--- T/settings.py
+++ T/settings.py
@@ -73,13 +73,17 @@ WSGI_APPLICATION = 'T.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'titoweb',
+        'USER': 'tito',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
