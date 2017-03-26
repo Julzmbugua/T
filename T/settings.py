5eaa6945c41e6a1ab12442f46b886a268aa30118
Julius Mbugua <julzmbugua@gmail.com>
2017-03-26 07:36:29 +0300

    still still on that portfolio upload shit

--- T/settings.py
+++ T/settings.py
@@ -121,10 +121,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
+    )
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config(default=os.getenv('DATABASE_URL'))
